9506ad8efd80b4850249563624ef97ba0065a7cb
fganga <fganga@unaj.edu.ar>
2023-08-25 13:24:55 -0300

    django templates.

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"os"
 
+	"github.com/flosch/pongo2/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
@@ -32,21 +32,14 @@ func main() {
 			"message": "Hello, World!",
 		})
 	})
-	templ, err := template.New("base.gohtml").ParseFiles("templates/base.gohtml")
-
-	if err != nil {
-		panic(err)
-	}
 
 	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
 
-		type TemplGlobals struct {
-			Mode string
-		}
+		templ := pongo2.Must(pongo2.FromFile("templates/base.html"))
 
-		err := templ.Execute(w, TemplGlobals{
-			Mode: appEnv,
-		})
+		err := templ.ExecuteWriter(pongo2.Context{
+			"mode": appEnv,
+		}, w)
 
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
